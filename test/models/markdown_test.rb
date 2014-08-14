2308350a33a59058b0aeb4513ba85ac6bc42369b
shibata <a1125ts@aiit.ac.jp>
2014-08-14 21:42:55 +0900

    test少しだけ追加

--- test/models/markdown_test.rb
+++ test/models/markdown_test.rb
@@ -4,9 +4,25 @@ require 'test_helper'
 class MarkdownTest < ActiveSupport::TestCase
   describe Markdown do
     describe '#preview' do
-      it "trueを返す" do
-        assert(true)
+      before do
       end
+
+      it "ベタ文字は<p>で囲むこと" do
+        markdown = Markdown.create!(title: "title1", content: 'hoge')
+        assert("<p>hoge</p>", markdown.preview)
+      end
+
+      it "#は<h1>に変換すること" do
+        markdown = Markdown.create!(title: "title1", content: '# hoge')
+        assert("<h1>hoge</h1>", markdown.preview)
+      end
+
+      it "_は<em>に変換しないこと" do
+        markdown = Markdown.create!(title: "title1", content: 'a_hoge_b')
+        assert("<a>a_hoge_b</>", markdown.preview)
+      end
+
+
     end
   end
 end
